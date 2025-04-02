e0fe904a7c8de8684fe9f4acbae2fe7fddcb03a2
beanavas <beanavasaguilera@gmail.com>
2025-04-02 14:38:49 -0400

    Gravity & floor collision

--- my-phaser-game/src/gameobjects/Player.js
+++ my-phaser-game/src/gameobjects/Player.js
@@ -1,5 +1,5 @@
 import { GameObjects, Physics } from "phaser";
-
+import { Scene } from "phaser";
 
 export class Player extends Physics.Arcade.Image {
     
@@ -8,6 +8,7 @@ export class Player extends Physics.Arcade.Image {
     propulsion_fire = null;
     scene = null;
     spaceKey = null;
+    lives = 5;
 
     constructor({ scene }) {
         super(scene, 200, 100, "player");
@@ -21,16 +22,12 @@ export class Player extends Physics.Arcade.Image {
         this.body.enable = false;
 
         // Bullets group to create pool
-        this.bullets = this.scene.physics.add.group({
-            // classType: Bullet,
-            // maxSize: 100,
-            // runChildUpdate: true
-        });
+        this.bullets = this.scene.physics.add.group();
 
         // Set up space key input for flapping
         this.spaceKey = this.scene.input.keyboard.addKey("SPACE");
 
-        //Bouncing before game starts
+        // Bouncing before game starts
         this.idleTween = this.scene.tweens.add({
             targets: this,
             y: this.y - 70,
@@ -41,32 +38,47 @@ export class Player extends Physics.Arcade.Image {
         });
 
         this.iddleTween = true;
+
+        // Create particles
+        
     }
 
     start() {
-
         this.state = "start";
 
-        //Stops bouncing 
-        if(this.iddleTween){
-        this.idleTween.stop();
-        this.idleTween = null;
+        // Stops bouncing 
+        if (this.iddleTween) {
+            this.idleTween.stop();
+            this.idleTween = null;
         }
 
         this.body.enable = true;
-        
         this.setVelocityY(-200);
         this.state = "can_move";
-    
     }
 
     update() {
         if (this.state === "can_move") {
-
             // If the space key is pressed, apply upward velocity
             if (Phaser.Input.Keyboard.JustDown(this.spaceKey)) {
                 this.setVelocityY(-200); 
             }
         }
+        if (this.body.blocked.down || this.body.touching.down) {
+            this.die();
+        }
+    }
+
+    die() {
+        if (this.state === "dead") return; 
+
+        this.state = "dead";
+        this.body.enable = false; 
+        this.setTint(0xff0000);
+
+        // Delay the scene transition to allow any effects to play
+        this.scene.time.delayedCall(1000, () => {
+            this.scene.scene.start("GameOverScene", { points: this.scene.points || 0 });
+        });
     }
 }
--- my-phaser-game/src/scenes/MainScene.js
+++ my-phaser-game/src/scenes/MainScene.js
@@ -1,15 +1,10 @@
 import { Scene } from "phaser";
 import { Player } from "../gameobjects/Player";
-// Removed BlueEnemy import
-// import { BlueEnemy } from "../gameobjects/BlueEnemy";
 
 export class MainScene extends Scene {
     player = null;
-    enemy_blue = null; // Remains null
-    cursors = null;
 
-    points = 0;
-    game_over_timeout = 20;
+    points = 5;
 
     constructor() {
         super("MainScene");
@@ -19,78 +14,61 @@ export class MainScene extends Scene {
         this.cameras.main.fadeIn(1000, 0, 0, 0);
         this.scene.launch("MenuScene");
 
-        // Reset points and timeout
-        this.points = 0;
-        this.game_over_timeout = 20;
+        // Use the lives or go to default if none
+        this.lives = this.data.lives ?? 5;
+
     }
 
     create() {
-        this.add.image(0, 0, "background")
-            .setOrigin(0, 0);
-        this.add.image(0, this.scale.height, "floor")
-            .setOrigin(0, 1);
+        // Create background image
+        this.add.image(0, 0, "background").setOrigin(0, 0);
+        
+        // Create the floor image and enable it as a static physics object
+        const floor = this.add.image(0, this.scale.height, "floor").setOrigin(0, 1);
+        this.physics.add.existing(floor, true); // true makes it static
 
         // Create the player
         this.player = new Player({ scene: this });
 
-        // Do NOT instantiate the blue enemy:
-        // this.enemy_blue = new BlueEnemy(this);
+        // Collider
+        this.physics.add.collider(this.player, floor, (player, floor) => {
 
-        // Set up cursor keys and input events
-        this.cursors = this.input.keyboard.createCursorKeys();
-        this.cursors.space.on("down", () => {
-            this.player.fire();
-        });
-        this.input.on("pointerdown", (pointer) => {
-            this.player.fire(pointer.x, pointer.y);
-        });
+            // Calculate the bottom of the player's body (using its y and half its height)
+            const playerBottom = player.y + player.displayHeight / 2;
 
-        // Remove overlap events related to the blue enemy:
-        // this.physics.add.overlap(this.player.bullets, this.enemy_blue, ...);
-        // this.physics.add.overlap(this.enemy_blue.bullets, this.player, ...);
+            // The top of the floor is at floor.y because the floor's origin is (0,1)
+            if (playerBottom >= floor.y - 5) { // 5-pixel tolerance
+                player.die();
+            }
+        });
 
         // This event comes from MenuScene
         this.game.events.on("start-game", () => {
             this.scene.stop("MenuScene");
             this.scene.launch("HudScene", { remaining_time: this.game_over_timeout });
             this.player.start();
-            // Remove blue enemy start call since it's not used:
-            // if (this.enemy_blue) {
-            //     this.enemy_blue.start();
-            // }
-
-            // Game Over timeout event
-            this.time.addEvent({
-                delay: 1000,
-                loop: true,
-                callback: () => {
-                    if (this.game_over_timeout === 0) {
-                        this.game.events.removeListener("start-game");
-                        this.scene.stop("HudScene");
-                        this.scene.start("GameOverScene", { points: this.points });
-                    } else {
-                        this.game_over_timeout--;
-                        this.scene.get("HudScene").update_timeout(this.game_over_timeout);
-                    }
-                }
-            });
         });
     }
 
+    handlePlayerDeath(){
+
+        this.lives--;
+
+        if(this.lives > 0){
+
+            this.scene.launch("HudScene");
+
+        } else {
+
+            this.scene.stop("HudScene");
+            this.scene.launch("GameOverScene");
+
+        }
+    }
+
     update() {
+
         this.player.update();
 
-        // Only update the blue enemy if it exists
-        if (this.enemy_blue) {
-            this.enemy_blue.update();
-        }
-
-        // Player movement entries
-        if (this.cursors.up.isDown) {
-            this.player.move("up");
-        }
-        if (this.cursors.down.isDown) {
-            this.player.move("down");
-        }
     }
 }
